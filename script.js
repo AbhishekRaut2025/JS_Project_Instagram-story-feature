cb337e01d751dbdc48596d4796a641ff133ae32a
Abhishek Raut <iamabhishekraut01@gmail.com>
2025-07-21 18:19:11 +0530

    Instagram Story feature

---
+++ script.js
@@ -0,0 +1,36 @@
+var arr = [
+    {
+        dp: "https://images.unsplash.com/photo-1488161628813-04466f872be2?q=80&w=764&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        story: "https://images.unsplash.com/photo-1751378679063-cbd774c0f973?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyMHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+        dp: "https://plus.unsplash.com/premium_photo-1692640261271-fca31c07292d?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        story: "https://images.unsplash.com/photo-1611874195190-409bddfb31fa?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDZ8fHxlbnwwfHx8fHw%3D"
+    },
+    {
+        dp: "https://images.unsplash.com/photo-1519085360753-af0119f7cbe7?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        story: "https://images.unsplash.com/photo-1576469196962-433b47333f9b?q=80&w=627&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+        dp: "https://images.unsplash.com/photo-1502791451862-7bd8c1df43a7?q=80&w=764&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        story: "https://images.unsplash.com/photo-1546484475-7f7bd55792da?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    }
+];
+
+var stories = document.querySelector("#stories");
+
+var clutter = "";
+arr.forEach(function (val, idx) {
+    clutter += `<div class="story">
+                <img id="${idx}" src="${val.dp}" alt="">
+               </div>`;
+})
+stories.innerHTML = clutter;
+
+stories.addEventListener("click", function (dets) {
+    document.querySelector("#fullScrean").style.display = "block";
+    document.querySelector("#fullScrean").style.backgroundImage = `url(${arr[dets.target.id].story})`;
+    setTimeout(function () {
+        document.querySelector("#fullScrean").style.display = "none";
+    }, 3000)
+})
